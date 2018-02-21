946d2a3595e603fcc675ce01ba1068be5d1ab8fa
Somayeh <mail.somayeh@gmail.com>
2018-02-21 18:46:25 +0000

    add new release_bike test and instantiate bike in DockingStation and update working method

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -5,4 +5,11 @@ describe DockingStation do
   #   expect(subject).to respond_to :release_bike
   # end
   it { is_expected.to respond_to(:release_bike)}
+
+
+  it 'should release a bike' do
+  bike = subject.release_bike
+  expect(bike).to be_working
+
+  end
 end
