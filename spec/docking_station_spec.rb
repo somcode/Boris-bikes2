002daa50f31051d58c5d86e2103caae17090853a
durranee <durranee@hotmail.com>
2018-02-22 14:46:07 +0000

    step 11, using instance var and attr_reader and dock_bike method test passed

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -4,12 +4,20 @@ describe DockingStation do
   # it 'responds to release_bike' do
   #   expect(subject).to respond_to :release_bike
   # end
-  it { is_expected.to respond_to(:release_bike)}
+  it { should respond_to(:release_bike)}
 
 
   it 'should release a bike' do
-  bike = subject.release_bike
-  expect(bike).to be_working
-
+    bike = subject.release_bike
+    expect(bike).to be_working
   end
+  it { should respond_to(:dock_bike) }
+  it { should respond_to(:dock_bike).with(1).arguments }
+
+  it 'checks if dock_bike actually takes a bike' do
+    #ds = DockingStation.new
+    bike = subject.release_bike
+    expect(subject.dock_bike(bike)).to be_an_instance_of(Bike)
+  end
+
 end
