f9f6ce7d999b913b228ebdb5f900819b12956af6
Somayeh <mail.somayeh@gmail.com>
2018-02-26 12:38:22 +0000

    updating tests

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -8,13 +8,13 @@ class DockingStation
   end
 
   def release_bike
-    raise "Staaaahp! there is no bike" if @bikes.empty?
+    raise "there is no bike" if @bikes.empty?
     @bikes.pop
   end
 
   def dock_bike(bike)
-    raise "there is no space" if @bikes.length == 20
+    raise "there is no space" if @bikes.length >= 20
     @bikes.push(bike)
-     #@bike = bike
+
   end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -4,28 +4,41 @@ describe DockingStation do
   # it 'responds to release_bike' do
   #   expect(subject).to respond_to :release_bike
   # end
+  subject(:docking_station) { described_class.new }
+
   it { should respond_to(:release_bike)}
 
   it 'should release a bike' do
-    bike = subject.release_bike
-    expect(bike).to be_working
+    bike = docking_station.release_bike
+    expect(bike.working?).to eq true
   end
 
-  it { should respond_to(:dock_bike) }
-  it { should respond_to(:dock_bike).with(1).arguments }
+  #it { should respond_to(:dock_bike) }
+  #it { should respond_to(:dock_bike).with(1).arguments }
 
   it 'checks if dock_bike actually takes a bike' do
-    #ds = DockingStation.new
-    bike = subject.release_bike
-    expect(subject.dock_bike(bike)).to be_an_instance_of(Bike)
+
+    bike = docking_station.release_bike
+    docking_station.dock_bike(bike)
+    expect(docking_station.bikes.include?(bike)).to eq true
+
   end
 
 
-  it 'release_bike should raise an error' do
-    expect { subject.release_bike }.to raise_error # 'there is no bike'
+  it 'release_bike should raise an error when is empty' do
+    20.times {docking_station.release_bike}
+    expect { docking_station.release_bike }.to raise_error 'there is no bike'
   end
 
-  it 'dock_bike should raise an error' do
-    expect {subject.dock_bike}.to raise_error
+  it 'dock_bike should raise an error when is full' do
+    fake_bike = double(
+      :working? => true
+    )
+
+    #19.times {subject.dock_bike(Bike.new)}
+    #p "BIKES: #{subject.bikes}"
+
+
+    expect { subject.dock_bike(bike) }.to raise_error 'there is no space'
   end
 end
